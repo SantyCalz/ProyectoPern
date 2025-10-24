2e52d12b9a9918f2cbb8bfa1ae51b9785e4b2661
Santiago Calzolari <santyxd321@gmail.com>
2025-10-24 18:16:28 -0300

    cambios en db.js

--- src/db.js
+++ src/db.js
@@ -1,17 +1,16 @@
 import pg from "pg";
-import { PG_PORT, PG_HOST, PG_USER, PG_PASSWORD, PG_DATABASE } from './config.js';
 
-// Named export `pool` and a default export for convenience.
-export const pool = new pg.Pool({
-    port: PG_PORT,
-    host: PG_HOST,
-    user: PG_USER,
-    password: PG_PASSWORD,
-    database: PG_DATABASE,
+const { Pool } = pg;
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL, // usar variable de entorno
+  ssl: {
+    rejectUnauthorized: false // necesario en Render
+  }
 });
 
 pool.on('connect', () => {
-    console.log('Conectado a la base de datos');
+  console.log('Conectado a la base de datos');
 });
 
 export const ORIGIN = process.env.ORIGIN || 'http://localhost:5173';
